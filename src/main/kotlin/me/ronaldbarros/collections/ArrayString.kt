13a87c177fc19e3c4a69adc47d788fb7ab1e3141
Ronald Barros <rsmbarros@gmail.com>
2021-06-20 22:01:22 -0300

    create array string example

--- src/main/kotlin/me/ronaldbarros/collections/ArrayString.kt
+++ src/main/kotlin/me/ronaldbarros/collections/ArrayString.kt
@@ -1,2 +1,26 @@
 package me.ronaldbarros.collections
 
+fun main() {
+    val names = Array(3) { "" }
+    names[0] = "Mary"
+    names[1] = "Theo"
+    names[2] = "John"
+
+    for (name in names){
+        println(name)
+    }
+
+    println("------")
+
+    names.sort()
+    names.forEach {
+        println(it)
+    }
+
+    val names2 = arrayOf("Mike", "William", "Jackie")
+    println("------")
+    names2.sort()
+    names2.forEach {
+        println(it)
+    }
+}
